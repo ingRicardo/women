3f96df5b9399d4d12442e838d3e253ed99e8154c
Riky <ra_mo116@hotmail.com>
2026-08-26 20:23:33 -0700

    add enviromental chatbot api k

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  geminiApiKey: 'AQ.Ab8RN6IHyFTiyii-1EuJklgzVgdoho_tpCNIxOnieZLRpYsChA',
+  apiUrl: 'https://webwomen-api.onrender.com/api' // Your Render API URL
+};
