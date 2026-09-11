fef84d0a05fb78678e9a7c89d936697fcd9118df
Riky <ra_mo116@hotmail.com>
2026-09-10 20:21:19 -0700

    edit woman done

--- src/app/components/womanmain/womanmain.ts
+++ src/app/components/womanmain/womanmain.ts
@@ -1,46 +1,81 @@
 import { ChangeDetectorRef, Component, computed, effect, ElementRef, inject, model, OnInit, signal, ViewChild } from '@angular/core';
-import {WomanService} from '../../services/woman.service';
+import { WomanService } from '../../services/woman.service';
 import { Woman } from '../models/woman.model';
-import {NgOptimizedImage} from '@angular/common';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule,Validators,  } from '@angular/forms'; 
+import { NgOptimizedImage } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-womanmain',
-  imports: [ NgOptimizedImage, FormsModule],
+  imports: [NgOptimizedImage, FormsModule],
   standalone: true,
   templateUrl: './womanmain.html',
   styleUrl: './womanmain.css',
 })
 
-export class Womanmain implements OnInit{
+export class Womanmain implements OnInit {
 
   @ViewChild('womanName') womanNameElement!: ElementRef<HTMLInputElement>;
-  
   @ViewChild('womanId') womanIdElement!: ElementRef<HTMLInputElement>;
-
   @ViewChild('womanAvatar') womanAvatarElement!: ElementRef<HTMLInputElement>;
   @ViewChild('womanStatus') womanStatusElement!: ElementRef<HTMLSelectElement>;
-  
   @ViewChild('womanBirthday') womanBirthdayElement!: ElementRef<HTMLInputElement>;
-   
   @ViewChild('womanCountry') womanCountryElement!: ElementRef<HTMLInputElement>;
-
   @ViewChild('womanRace') womanRaceElement!: ElementRef<HTMLSelectElement>;
-  
   @ViewChild('womanEmail') womanEmailElement!: ElementRef<HTMLInputElement>;
-  
   @ViewChild('womanAge') womanAgeElement!: ElementRef<HTMLInputElement>;
 
-  editWoman() {
-   this.isEdit.set(true);
-  }
-  canceEditWoman(){
-    this.isEdit.set(false);
-  }
-  
-  updateWoman(){
+  private womenService = inject(WomanService);
+  women = signal<Woman[]>([]);
+  isLoading = signal<boolean>(false);
+  isAddopen = signal<boolean>(false);
+  isEdit = signal<boolean>(false);
+  isWomanDisplayed = signal<boolean>(true);
+  alertMessage = signal<string | null>(null);
+  alertType = signal<'success' | 'error' | null>(null);
 
-    if(this.womanNameElement && this.womanIdElement){
+  name = model('');
+  avatar = model('');
+  age = model(0);
+  status = model('');
+  dateOfBirth = model('');
+  country = model('');
+  race = model('');
+  email = model('');
+
+  currentPage = signal(1);
+  pageSize = signal(5);
+
+  totalPages = computed(() => Math.ceil(this.women().length / this.pageSize()));
+
+  selectedWoman = signal<Woman | null>(null);
+
+  ngOnInit(): void {
+    this.loadWomen();
+  }
+  constructor() {
+    effect(() => {
+      const currentWomenList = this.women();
+      console.log('The women list has changed!', currentWomenList);
+      this.isLoading.set(false);
+    });
+  }
+
+  editWoman() {
+    this.isEdit.set(true);
+  }
+  canceEditWoman() {
+    this.isEdit.set(false);
+    this.isWomanDisplayed.set(false);
+  }
+
+  updateWoman() {
+
+    if (this.womanNameElement.nativeElement.value
+      && this.womanAvatarElement.nativeElement.value && +this.womanAgeElement.nativeElement.value > 0
+      && this.womanStatusElement.nativeElement.value && this.womanRaceElement.nativeElement.value
+      && this.womanBirthdayElement.nativeElement.value && this.womanCountryElement.nativeElement.value
+      && this.womanEmailElement.nativeElement.value
+    ) {
       const womanNameValue = this.womanNameElement.nativeElement.value;
       const womanIDValue = +this.womanIdElement.nativeElement.value;
       const womanAvatarValue = this.womanAvatarElement.nativeElement.value;
@@ -50,156 +85,130 @@ export class Womanmain implements OnInit{
       const womanCountryValue = this.womanCountryElement.nativeElement.value;
       const womanRaceValue = this.womanRaceElement.nativeElement.value;
       const womanEmailValue = this.womanEmailElement.nativeElement.value;
-     // console.log(womanIDValue, womanNameValue, womanAvatarValue, womanStatusValue, womanBirthValue, womanCountryValue, womanRaceValue, womanEmailValue, womanAgeValue);
-        const payload= {
-          id: womanIDValue,
-          name: womanNameValue, // Fallback to an empty string if null/undefined
-          avatar: womanAvatarValue ,
-          age: womanAgeValue,
-          status:womanStatusValue,
-          dateOfBirth: womanBirthValue,
-          country: womanCountryValue,
-          race: womanRaceValue,
-          email: womanEmailValue
+
+      const payload = {
+        id: womanIDValue,
+        name: womanNameValue, // Fallback to an empty string if null/undefined
+        avatar: womanAvatarValue,
+        age: womanAgeValue,
+        status: womanStatusValue,
+        dateOfBirth: womanBirthValue,
+        country: womanCountryValue,
+        race: womanRaceValue,
+        email: womanEmailValue
       };
-       console.log(payload);
+      console.log(payload);
       this.womenService.updateWomanV1(womanIDValue, payload).subscribe({
         next: (response) => {
           console.log("women updated succesfully!", response);
-           this.showAlert("Woman updated successfully!", "success");
-           this.loadWomen();
-           this.isLoading.set(true);
-        },
-         error: (error) => {
+          this.showAlert("Woman updated successfully!", "success");
           this.loadWomen();
+          this.isLoading.set(true);
+          this.isEdit.set(false);
+          this.isWomanDisplayed.set(false);
+
+        },
+        error: (error) => {
+          this.loadWomen();
+          this.isEdit.set(false);
+          this.isWomanDisplayed.set(false);
           console.error('Update failed', error);
           this.showAlert("Error updating profile. Please try again.", "error");
-         }
+        }
       })
-    
+
     }
-      this.isLoading.set(false);
-      this.loadWomen();
+    this.isLoading.set(false);
+    this.isEdit.set(false);
+    this.isWomanDisplayed.set(false);
+    this.loadWomen();
   }
 
   openAdd() {
     this.isAddopen.set(true);
   }
-  closeAdd(){
+  closeAdd() {
     this.isAddopen.set(false);
   }
 
-  private womenService = inject(WomanService);
-  women = signal<Woman[]>([]);
-  isLoading = signal<boolean>(false);
-  isAddopen = signal<boolean>(false);
-  isEdit = signal<boolean>(false);
+  showAlert(message: string, type: 'success' | 'error') {
+    this.alertMessage.set(message);
+    this.alertType.set(type);
 
-   ngOnInit(): void {
+    setTimeout(() => {
+      this.alertMessage.set(null);
+      this.alertType.set(null);
+    }, 5000);
+  }
+
+  saveWoman() {
+    const payload: Omit<Woman, "id"> = {
+      name: this.name(), // Fallback to an empty string if null/undefined
+      avatar: this.avatar(),
+      age: this.age(),
+      status: this.status(),
+      dateOfBirth: this.dateOfBirth(),
+      country: this.country(),
+      race: this.race(),
+      email: this.email()
+    };
+
+    console.log("payload", payload);
+    this.womenService.createWomanv1(payload).subscribe({
+      next: (response) => {
+        console.log("women created succesfully!", response);
+        this.showAlert("Woman created successfully!", "success");
+        this.name.set('');
+        this.avatar.set('');
+        this.age.set(0);
+        this.status.set('');
+        this.dateOfBirth.set('');
+        this.country.set('');
+        this.race.set('');
+        this.email.set('');
+        this.isEdit.set(false);
+        this.loadWomen();
+        this.isLoading.set(true);
+
+      },
+      error: (error) => {
+        this.loadWomen();
+        console.error('Registration failed', error);
+        this.showAlert("Error creating profile. Please try again.", "error");
+      }
+    });
+    this.isLoading.set(false);
+    this.isEdit.set(false);
     this.loadWomen();
-   }
-    constructor( ) {
-       effect(() => {
-        const currentWomenList = this.women();
-        console.log('The women list has changed!', currentWomenList);
-        this.isLoading.set(false);
-      });
-    }
-     
-    alertMessage = signal<string | null>(null);
-    alertType = signal<'success' | 'error' | null>(null);
 
-     
-    showAlert(message: string, type: 'success' | 'error') {
-      this.alertMessage.set(message);
-      this.alertType.set(type);
-
-      setTimeout(() => {
-        this.alertMessage.set(null);
-        this.alertType.set(null);
-      }, 5000);
-    }
-    name = model('');
-    avatar = model('');
-    age = model(0);
-    status = model('');
-    dateOfBirth = model('');
-    country = model('');
-    race = model('');
-    email = model('');
-    
-   saveWoman() {
-      const payload: Omit<Woman, "id"> = {
-        name: this.name(), // Fallback to an empty string if null/undefined
-        avatar: this.avatar() ,
-        age: this.age(),
-        status:this.status(),
-        dateOfBirth: this.dateOfBirth(),
-        country: this.country(),
-        race: this.race(),
-        email: this.email()
-      };
-
-      console.log("payload", payload);
-      this.womenService.createWomanv1(payload).subscribe({
-        next: (response) => {
-          console.log("women created succesfully!", response);
-          this.showAlert("Woman created successfully!", "success");
-            this.name.set('');
-            this.avatar.set('');
-            this.age.set(0);
-            this.status.set('');
-            this.dateOfBirth.set('');
-            this.country.set('');
-            this.race.set('');
-            this.email.set('');
-          this.isEdit.set(false);
-          this.loadWomen();
-          this.isLoading.set(true);
-
-        },
-        error: (error) => {
-          this.loadWomen();
-          console.error('Registration failed', error);
-          this.showAlert("Error creating profile. Please try again.", "error");
-         }
-      });
-      this.isLoading.set(false);
-      this.isEdit.set(false);
-      this.loadWomen();
- 
-   }
-   loadWomen() {
+  }
+  loadWomen() {
     console.log('Fetching women data...');
     this.womenService.getWomenv1().subscribe({
       next: (data) => {
         this.women.set(data);
         console.log('Women data fetched:', this.women());
 
-       },
+      },
       error: (err) => {
         console.error('Error fetching women:', err);
       }
     });
     this.isLoading.set(false);
-    }
+  }
 
-    checkifloading(){
+  checkifloading() {
     if (this.isLoading())
       return true;
     else return false;
-   }
+  }
 
-  currentPage = signal(1);
-  pageSize = signal(5);
-
-   paginatedItems = computed(() => {
+  paginatedItems = computed(() => {
     const start = (this.currentPage() - 1) * this.pageSize();
     const end = start + this.pageSize();
     return this.women().slice(start, end);
   });
 
-   totalPages = computed(() => Math.ceil(this.women().length / this.pageSize()));
 
   nextPage() {
     if (this.currentPage() < this.totalPages()) {
@@ -213,24 +222,23 @@ export class Womanmain implements OnInit{
     }
   }
 
-  firstPage(){
-    if(this.currentPage() >= 1){
+  firstPage() {
+    if (this.currentPage() >= 1) {
       this.currentPage.update(p => 1);
     }
   }
-  lastPage(){
-    if(this.currentPage() >= 1){
+  lastPage() {
+    if (this.currentPage() >= 1) {
       this.currentPage.update(p => this.totalPages());
     }
   }
 
-  selectedWoman = signal<Woman | null>(null);
-
   onRowClick(rowData: Woman): void {
     this.isEdit.set(false);
+    this.isWomanDisplayed.set(true);
     console.log('Row Data Captured:', rowData);
     this.selectedWoman.set(rowData); // Update the signal state
-   }
+  }
 
- 
+
 }
